c300cbad7f714daefbdc6d2ee2daf31e7a441809
sncandiani <sofiancandiani@gmail.com>
2020-04-12 22:30:57 -0500

    completed first and second exercises

--- dictionaryOfWords.py
+++ dictionaryOfWords.py
@@ -4,25 +4,28 @@ represent words (key) and its definition (value)
 """
 word_definitions = dict()
 
-"""
-Add several more words and their definitions
-   Example: word_definitions["Awesome"] = "The feeling of students when they are learning Python"
-"""
 
-"""
-Use square bracket lookup to get the definition of two
-words and output them to the console with `print()`
-"""
-
-
-"""
-Loop over the dictionary to get the following output:
-    The definition of [WORD] is [DEFINITION]
-    The definition of [WORD] is [DEFINITION]
-    The definition of [WORD] is [DEFINITION]
-"""
-
-word_definitions["Awesome"] = "The feeling of students when they are learning Python"
+word_definitions = {
+"solvent": "capable of dissolving another substance", 
+"digress": "To turn aside, especially to depart temporarily from the main subject in writing or speaking"
+}
 
 for word in word_definitions:
     print(f"The definition of {word} is {word_definitions[word]}")
+
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+
+for idiom in idioms: 
+    space = " "
+    print(f"{idiom}: {space.join(idioms[idiom])}")
